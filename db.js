f06ac09298181be0122fafe5de3ae1e963c3ad8d
Pete8219 <psavrasov@gmail.com>
2020-06-17 20:48:08 +0500

    Добавлены шаблоны страниц services,employees,create-service,  а также маршруты в файле router.js

--- db.js
+++ db.js
@@ -2,12 +2,16 @@ const dotenv = require("dotenv")
 dotenv.config()
 const mongodb = require("mongodb")
 
-mongodb.connect(process.env.CONNECTIONSTRING, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}, function (err, client) {
-  console.log("you are connected")
-  module.exports = client
-  const app = require("./app")
-  app.listen(process.env.PORT)
-})
+mongodb.connect(
+  process.env.CONNECTIONSTRING,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  },
+  function(err, client) {
+    console.log("you are connected")
+    module.exports = client
+    const app = require("./app")
+    app.listen(process.env.PORT)
+  }
+)
--- router.js
+++ router.js
@@ -1,38 +1,37 @@
-const express = require('express')
+const express = require("express")
 const router = express.Router()
 
 //обработка машрутов во frontend
-router.get('/', function (req, res) {
-  res.render('home-page')
+router.get("/", function(req, res) {
+  res.render("home-page")
 })
-
-
-
-
-
 
 //маршруты административной части
 
-router.get('/admin', function (req, res) {
-  res.render('admin/admin-page')
+router.get("/admin", function(req, res) {
+  res.render("admin/admin-page")
 })
 
 //управление сотрудниками
-router.get('/employees', function (req, res) {
-  res.send("Здесь будет все, что касается настроек сотрудников")
+router.get("/employees", function(req, res) {
+  res.render("admin/employees")
 })
 
 //управление разделом Услуги
-router.get('/services', function (req, res) {
-  res.send('Раздел настройки услуг')
+router.get("/services", function(req, res) {
+  res.render("admin/services")
+})
+router.get("/create-service", function(req, res) {
+  res.render("admin/create-service")
+})
+
+router.post("/create-service", function(req, res) {
+  res.send("Data successfully saved")
 })
 
 //управление разделом Талоны
-router.get('/tickets', function (req, res) {
-  res.render('admin/tickets')
+router.get("/tickets", function(req, res) {
+  res.render("admin/tickets")
 })
-
-
-
 
 module.exports = router
